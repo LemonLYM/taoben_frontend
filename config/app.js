7b8b3b56cb739add742763aa3df5425ec232821a
luoyi <784554184@qq.com>
2020-12-02 23:53:45 +0800

    api

--- config/app.js
+++ config/app.js
@@ -1,15 +1,15 @@
 
 module.exports = {
 	// 请求域名 格式： https://您的域名
-	// HTTP_REQUEST_URL:'http://192.168.1.26:8324',
+	HTTP_REQUEST_URL:'http://apis.taoben888.cn/',
 	// 请求域名 格式： https://您的域名
 	 // #ifdef MP
-	  HTTP_REQUEST_URL:'',
+	  // HTTP_REQUEST_URL:'',/
 	 // #endif
 	 
 	 // #ifdef H5
 	  //H5接口是浏览器地址
-	  HTTP_REQUEST_URL: window.location.protocol+"//"+window.location.host,
+	  // HTTP_REQUEST_URL: window.location.protocol+"//"+window.location.host,
 	 // #endif
 	
 	HEADER:{

2ed86186f764620866ec241620722832801a854c
luoyi <784554184@qq.com>
2020-12-09 20:29:22 +0800

    feat:上传身份证功能

--- pages/users/user_phone/index.vue
+++ pages/users/user_phone/index.vue
@@ -6,14 +6,32 @@
 					<view class="item">
 						<input type='number' placeholder='填写手机号码' placeholder-class='placeholder' v-model="phone"></input>
 					</view>
+					
 					<view class="item acea-row row-between-wrapper">
 						<input type='number' placeholder='填写验证码' placeholder-class='placeholder' class="codeIput" v-model="captcha"></input>
 						<button class="code font-color" :class="disabled === true ? 'on' : ''" :disabled='disabled' @click="code">
 							{{ text }}
 						</button>
 					</view>
+					<view class="item-new no-border id-card">
+						<view class='acea-row row-middle'>
+							<view class="item-title">请上传身份证正反面照片</view>
+							<view class="item-desc">(图片格式支持JPG、PNG、JPEG)</view>
+							<view class="upload">
+								<view class='pictrue' v-for="(item,index) in pics" :key="index" :data-index="index" @click="getPhotoClickIdx">
+									<image :src='item'></image>
+									<text class='iconfont icon-guanbi1' @click.stop='DelPic(index)'></text>
+								</view>
+								<view class='pictrue acea-row row-center-wrapper row-column' @click='uploadpic' v-if="pics.length < 6">
+									<text class='iconfont icon-icon25201'></text>
+									<view>上传图片</view>
+								</view>
+							</view>
+						</view>
+					</view>
 				</view>
-				<button form-type="submit" class="confirmBnt bg-color">确认绑定</button>
+
+				<button form-type="submit" class="confirmBnt bg-color">认证</button>
 			</view>
 		</form>
 		<!-- #ifdef MP -->
@@ -51,7 +69,8 @@
 				captcha:'',
 				isAuto: false, //没有授权的不会自动授权
 				isShowAuth: false, //是否隐藏授权
-				key: ''
+				key: '',
+				pics:[] //上传的身份证的正反面照片
 			};
 		},
 		computed: mapGetters(['isLogin']),
@@ -71,6 +90,38 @@
 			}
 		},
 		methods: {
+			//点击上传照片
+			uploadpic: function() {
+				let that = this;
+				that.$util.uploadImageOne({url:'upload/image',count:6}, function(res) {
+					console.log(res);
+					that.pics.push(res.data.path);
+					that.$set(that, 'pics', that.pics);
+				});
+			
+			},
+			getPhotoClickIdx(e) {
+				let _this = this;
+				let idx = e.currentTarget.dataset.index;
+				_this.imgPreview(_this.pics, idx);
+			},
+			// 图片预览
+			imgPreview: function(list, idx) {
+				// list：图片 url 数组
+				if (list && list.length > 0) {
+					uni.previewImage({
+						current: list[idx], //  传 Number H5端出现不兼容 
+						urls: list
+					});
+				}
+			},
+			//删除照片
+			DelPic: function(index) {
+				let that = this,
+					pic = this.pics[index];
+				that.pics.splice(index, 1);
+				that.$set(that, 'pics', that.pics);
+			},
 			onLoadFun:function(){
 				this.isShowAuth = false;
 			},
@@ -183,6 +234,9 @@
 		font-weight: bold;
 		text-align: center;
 		margin-top: 55rpx;
+		width: 150rpx;
+		border: 1px solid #fff;
+		
 	}
 
 	.ChangePassword .list {
@@ -228,5 +282,89 @@
 		margin: 92rpx auto 0 auto;
 		text-align: center;
 		line-height: 90rpx;
+	}
+	.ChangePassword{
+		.item-new{
+			margin-top: 24rpx;
+		}
+		.no-border {
+			border-bottom: none;
+			padding-left: 0;
+			padding-right: 0;
+		}
+		.id-card{
+			.item-title{
+				color: #B2B2B2;
+				font-size: 32rpx;
+			}
+			.item-desc{
+				color: #B2B2B2;
+				font-size: 22rpx;
+				margin-top: 9rpx;
+				line-height: 36rpx;
+				width: 100%;
+			}
+			.upload {
+				display: -webkit-box;
+				display: -moz-box;
+				display: -webkit-flex;
+				display: -ms-flexbox;
+				display: flex;
+				-webkit-box-lines: multiple;
+				-moz-box-lines: multiple;
+				-o-box-lines: multiple;
+				-webkit-flex-wrap: wrap;
+				-ms-flex-wrap: wrap;
+				flex-wrap: wrap;
+				margin-top: 20rpx;
+			}
+			.pictrue{
+				width: 130rpx;
+				height: 130rpx;
+				margin: 24rpx 22rpx 0 0;
+				position: relative;
+				font-size: 11px;
+				color: #bbb;
+				
+				&:nth-child(4n) {
+					margin-right: 0;
+				}
+				
+				&:nth-last-child(1) {
+					border: 0.5px solid #ddd;
+					box-sizing: border-box;
+				}
+				
+				
+				uni-image,
+				image {
+					width: 100%;
+					height: 100%;
+					border-radius: 1px;
+				
+					img {
+						-webkit-touch-callout: none;
+						-webkit-user-select: none;
+						-moz-user-select: none;
+						display: block;
+						position: absolute;
+						top: 0;
+						left: 0;
+						opacity: 0;
+						width: 100%;
+						height: 100%;
+					}
+				}
+				
+				.icon-guanbi1 {
+					font-size: 33rpx;
+					position: absolute;
+					top: -10px;
+					right: -10px;
+				}
+			}
+
+		}
+
 	}
 </style>
